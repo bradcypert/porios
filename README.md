c4a87c7200cb67f801c769cd3f465c27e7b3708a
Brad Cypert <brad.cypert@gmail.com>
2015-11-04 20:08:48 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Porios
+##Where Podcasts Live
+
+This is a really basic sample readme just to get something in GIT. Add to this later.
---
+++ app/test/app/test/db/core.clj
@@ -0,0 +1,33 @@
+(ns app.test.db.core
+  (:require [app.db.core :as db]
+            [app.db.migrations :as migrations]
+            [clojure.test :refer :all]
+            [clojure.java.jdbc :as jdbc]
+            [conman.core :refer [with-transaction]]
+            [environ.core :refer [env]]))
+
+(use-fixtures
+  :once
+  (fn [f]
+    (db/connect!)
+    (migrations/migrate ["migrate"])
+    (f)))
+
+(deftest test-users
+  (with-transaction [t-conn db/*conn*]
+    (jdbc/db-set-rollback-only! t-conn)
+    (is (= 1 (db/create-user!
+               {:id         "1"
+                :first_name "Sam"
+                :last_name  "Smith"
+                :email      "sam.smith@example.com"
+                :pass       "pass"})))
+    (is (= [{:id         "1"
+             :first_name "Sam"
+             :last_name  "Smith"
+             :email      "sam.smith@example.com"
+             :pass       "pass"
+             :admin      nil
+             :last_login nil
+             :is_active  nil}]
+           (db/get-user {:id "1"})))))
