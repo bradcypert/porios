fc653c4995dcda0f37636ed3eb3039647a340228
Brad Cypert <brad.cypert@gmail.com>
2015-11-22 19:47:53 -0800

    adding profiles

---
+++ app/profiles.clj
@@ -0,0 +1,2 @@
+{:profiles/dev  {:env {:database-url "jdbc:postgresql://localhost/porios?user=dev&password=dev"}}
+ :profiles/test {:env {:database-url "jdbc:postgresql://localhost/porios?user=test&password=test"}}}
